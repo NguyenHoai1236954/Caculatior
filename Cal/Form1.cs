be0535a70bac24ce6d3e5502d21a183b5f514400
NguyenHoai1236954 <2051012028hoai@ou.edu.vn>
2023-11-03 11:14:39 +0700

    met qua feature1

--- Cal/Form1.cs
+++ Cal/Form1.cs
@@ -24,10 +24,13 @@ namespace Cal
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double a = double.Parse(textBox1.Text);
-           double b = double.Parse(textBox2.Text);
-            double c = a + b;
-            textBox3.Text = c.ToString();
+            if (textBox1.Text != null && textBox2.Text != null)
+            {
+                double a = double.Parse(textBox1.Text);
+                double b = double.Parse(textBox2.Text);
+                double c = a + b;
+                textBox3.Text = c.ToString();
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
